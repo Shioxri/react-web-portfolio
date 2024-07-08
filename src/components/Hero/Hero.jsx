48a73c7c9aea8f042dc2342d6a32e2e32457d839
Shioxri <troybelardo@gmail.com>
2024-07-08 18:07:52 +0800

    adjusted the link

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,6 @@ import { getImageUrl } from '../../utils'
 import styles from "./Hero.module.css"
 
 export const Hero = () => {
-  const pdfUrl = "./assets/hero/GVB_Portfolio.pdf";
   return (
     <section className={styles.container}>
         <div className={styles.content}>
@@ -20,7 +19,7 @@ export const Hero = () => {
             </p>
             <div className={styles.buttons}>
                 <a className={styles.contactBtn} href="mailto:gv.belardo@gmail.com">Contact Me</a>
-                <a className={styles.downloadBtn} href={pdfUrl} download value="GVB Portfolio"> Download CV</a>
+                <a className={styles.downloadBtn} href="assets/hero/GVB_Portfolio.pdf" download> Download CV</a>
             </div>
 
         </div>
